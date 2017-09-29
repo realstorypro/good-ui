f0d19ab5de8d460318a46cada7e1a98f6e27a5ca
Leonid Medovyy <lmedovyy@gmail.com>
2017-09-29 15:42:28 -0700

    updates to readme

--- spec/configuration/configuration_spec.rb
+++ spec/configuration/configuration_spec.rb
@@ -1,9 +1,9 @@
-describe DcUi::Configuration, 'configuration' do
+describe DcUi::Configuration, 'configuration', boot: false do
   it 'should not boot up without implementation file passed' do
     expect { DcUi.boot }.to raise_error(RuntimeError)
   end
 
-  it 'should boot with configuration file passed' do
+  it 'should boot with configuration file passed', boot: false do
     expected = expect do
       DcUi.configure do |config|
         config.ui_file = "#{DcUi.root}/lib/shared/ui.yml"
