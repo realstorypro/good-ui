a8e27110c0b18c68d1158f993e1b388dc425624d
Leonid Medovyy <lmedovyy@gmail.com>
2017-10-01 16:10:30 -0700

    building a component

--- lib/dc_ui/component.rb
+++ lib/dc_ui/component.rb
@@ -1,10 +1,110 @@
-# frozen_string_literal: true
-
 module DcUi
   # responsible for the out building of the component
   class Component
-    def initialize(_settings)
+    attr_accessor :tag, :css_class, :id, :url, :img, :text, :data, :style
+
+    def initialize(settings)
       @utils = Utilities.instance
+      @settings = settings
+      @id = nil
+      @url = nil
+      @img = nil
+      @text = nil
+      @data = nil
+      @style = nil
+      @css_class = ''
+      build
     end
+
+    def build
+      build_ui
+      build_dynamic
+      build_class
+      build_responsiveness
+      build_name
+      build_data
+      build_default_class
+    end
+
+    # builds out the ui class for the component
+    def build_ui
+      add_class 'ui' unless off?(:ui)
+    end
+
+    # adds dynamic class to the component is the switch is set
+    def build_dynamic
+      add_class 'dynamic' if on?(:dynamic)
+    end
+
+    # builds out css class for the component
+    def build_class
+      add_class @settings[:class] if @settings.key?(:class)
+    end
+
+    # builds out a default class for the component
+    def build_default_class
+      add_class @settings[:css_class] if @settings.key?(:css_class)
+    end
+
+    # builds out the reponsive css classes
+    def build_responsiveness
+      add_class build_only if @settings.key?(:only)
+      add_class build_size if @settings.key?(:size)
+      add_class build_size(:computer) if @settings.key?(:computer)
+      add_class build_size(:tablet) if @settings.key?(:tablet)
+      add_class build_size(:mobile) if @settings.key?(:mobile)
+    end
+
+    # builds out the name for the compnent
+    def build_name
+      if @settings.key?(:name)
+        add_class @settings[:name]
+        add_data :name, @settings[:name].parameterize.underscore
+      end
+    end
+
+    # builds data for the component
+    def build_data
+      @data = @settings[:data]
+    end
+
+    private
+
+    # appends class to the css_clss object
+    def add_class(klass)
+      css_class << ' ' + klass
+    end
+
+    # builds out the class markup for 'only' type display
+    def build_only
+      "#{@settings[:only]} only"
+    end
+
+    # builds out the class markup for the size
+    def build_size(device = nil)
+      return @utils.number_in_words(@settings[:size]) if device.equal? nil
+      "#{@utils.number_in_words(@settings[device])} wide #{device}"
+    end
+
+    # appds items to the data hash
+    def add_data(name, value)
+      # intialize data hash if one doesn't exist
+      @settings[:data] = {} if @settings[:data].nil?
+
+      @settings[:data][name] = value
+    end
+
+    # checks if the key is off
+    def off?(key)
+      return true if @settings[key].equal? :off
+      false
+    end
+
+    # checks if the key is on
+    def on?(key)
+      return true if @settings[key].equal? :on
+      false
+    end
+
   end
 end
--- spec/dc_ui/component_spec.rb
+++ spec/dc_ui/component_spec.rb
@@ -1,6 +1,87 @@
 # frozen_string_literal: true
 
-describe DcUi::Component, 'component' do
-  it 'it loads default values component' do
+describe DcUi::Component, 'component creation' do
+  it 'it can be created' do
+    component = DcUi::Component.new({})
+    expect(component).to be_a_kind_of(DcUi::Component)
   end
+end
+
+describe DcUi::Component, 'component abilities' do
+  before(:each) do
+    @utils = DcUi::Utilities.instance
+    @settings = @utils.merge_defaults('grid', class: 'round', text: 'test')
+    @component = DcUi::Component.new(@settings)
+  end
+
+  after(:each) do
+    @settings = nil
+    @component = nil
+  end
+
+  it 'is an instance of component' do
+    expect(@component).to be_an_instance_of(DcUi::Component)
+  end
+
+  it 'applies a ui key by default' do
+    expect(@component.css_class).to include('ui')
+  end
+
+  it 'renders a default component class' do
+    expect(@component.css_class).to include('grid')
+  end
+
+  it 'renders a passed class' do
+    expect(@component.css_class).to include('round')
+  end
+
+  it 'allows for a ui key to be turned off' do
+    settings = @utils.merge_defaults('grid', class: 'round', ui: :off)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to_not include('ui')
+  end
+
+  it 'adds a dynamic css class' do
+    settings = @utils.merge_defaults('grid', class: 'round', dynamic: :on)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('dynamic')
+  end
+
+  it 'can build a responsive column' do
+    settings = @utils.merge_defaults('column', size: 16)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('sixteen')
+  end
+
+  it 'can build a responsive mobile column' do
+    settings = @utils.merge_defaults('column', mobile: 6)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('six wide mobile')
+  end
+
+  it 'can build a responsive tablet column' do
+    settings = @utils.merge_defaults('column', tablet: 5)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('five wide tablet')
+  end
+
+  it 'can build a responsive computer column' do
+    settings = @utils.merge_defaults('column', computer: 2)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('two wide computer')
+  end
+
+  it 'can build a computer only column' do
+    settings = @utils.merge_defaults('column', only: :computer)
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('computer only')
+  end
+
+  it 'accepts name and generates class & data' do
+    settings = @utils.merge_defaults('column', name: 'sidebar')
+    component = DcUi::Component.new(settings)
+    expect(component.css_class).to include('sidebar')
+    expect(component.data.name).to include('sidebar')
+  end
+
 end
